679ee498a8e14e9c944c73634ba01d382897d794
Вова Бунтман <bunt@yandex.ru>
2025-09-05 10:44:26 +0300

    refactor: Remove nested card from dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -376,11 +376,7 @@ export default function DashboardPage() {
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           {/* Create New Summary */}
           <div className="lg:col-span-2 space-y-6">
-            <Card>
-              <CardContent>
-                <UrlForm onSubmit={handleSummarize} isLoading={isProcessing} />
-              </CardContent>
-            </Card>
+            <UrlForm onSubmit={handleSummarize} isLoading={isProcessing} />
 
             <SummaryDisplay 
               summary={summary} 
